21de2fa62403d6653205e5cbaa7392520c54da5c
gonzalovazace <gonvazace@gmail.com>
2024-06-24 12:12:56 -0300

    archivos con json

--- ProyectoFinalLaboratorio3/src/main/java/personas/GestorPersonas.java
+++ ProyectoFinalLaboratorio3/src/main/java/personas/GestorPersonas.java
@@ -6,15 +6,21 @@ import exceptionsPersonalizadas.EmailOContraseniaIncorrectos;
 import jdk.jshell.spi.SPIResolutionException;
 
 import java.io.*;
+import java.lang.reflect.Type;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class GestorPersonas {
 
     private HashSet<Persona> personas;
+    private Gson gson;
 
     public GestorPersonas() {
+        this.gson = gson;
         this.personas= new HashSet<>();
     }
 
@@ -75,48 +81,26 @@ public class GestorPersonas {
     }
 
     public void cargarDatos() {
-        try (BufferedReader br = new BufferedReader(new FileReader("usuarios.txt"))) {
-            String linea;
-            while ((linea = br.readLine()) != null) {
-                String[] datos = linea.split(",");
-                if (datos.length > 6) {
-                    String tipo = datos[0];
-                    String nombre = datos[1];
-                    String apellido = datos[2];
-                    String dni = datos[3];
-                    String email = datos[4];
-                    String usuario = datos[5];
-                    String contrasenia = datos[6];
-
-                    if (tipo.equals("Cliente") && datos.length == 12) {
-                        String pais = datos[7];
-                        String provincia = datos[8];
-                        String ciudad = datos[9];
-                        String direccion = datos[10];
-                        int codigoPostal = Integer.parseInt(datos[11]);
-                        Persona persona = new Cliente(nombre, apellido, dni, email, usuario, contrasenia, pais, provincia, ciudad, direccion, codigoPostal);
-                        personas.add(persona);
-                    } else if (tipo.equals("Administrador") && datos.length == 8) {
-                        String legajo = datos[7];
-                        Persona persona = new Administrador(nombre, apellido, dni, email, usuario, contrasenia);
-                        personas.add(persona);
-                    }
-                }
+        try (BufferedReader br = new BufferedReader(new FileReader("usuarios.json"))) {
+            Type setType = new TypeToken<Set<Persona>>() {}.getType();
+            personas = gson.fromJson(br, setType);
+            if (personas == null) {
+                personas = new HashSet<>();
             }
+        } catch (FileNotFoundException e) {
+            System.out.println("Archivo no encontrado, creando nuevo archivo...");
+            personas = new HashSet<>();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void guardarDatos() {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter("usuarios.txt"))) {
-            for (Persona persona : personas) {
-                bw.write(persona.toString());
-                bw.newLine();
-            }
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("usuarios.json"))) {
+            gson.toJson(personas, bw);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+}
 
-
-    }}
